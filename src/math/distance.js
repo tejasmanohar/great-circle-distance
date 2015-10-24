dd69a1a2998c5c42c4eb448cd181f078be03e2d2
Tejas Manohar <me@tejas.io>
2015-10-24 06:34:50 -0500

    cordX -> cX for simplicity

--- src/math/distance.js
+++ src/math/distance.js
@@ -1,21 +1,21 @@
 
 /**
  * Returns central angle between two cordinates
- * @param {Object[]} cord1 - 1st [lat, lon] pair
- * @param {Object[]} cord2 - 2nd [lat, lon] pair
+ * @param {Object[]} c1 - 1st [lat, lon] pair
+ * @param {Object[]} c2 - 2nd [lat, lon] pair
  */
-function getCentralAngle(cord1, cord2) {
-  return Math.sin(cord2[0] - cord1[0] / 2) ** 2 +
-         Math.cos(cord1[0]) * Math.cos(cord2[0]) *
-         Math.sin(cord2[1] - cord1[0] / 2) ** 2
+function getCentralAngle(c1, c2) {
+  return Math.sin(c2[0] - c1[0] / 2) ** 2 +
+         Math.cos(c1[0]) * Math.cos(c2[0]) *
+         Math.sin(c2[1] - c1[1] / 2) ** 2
 }
 
 /**
  * Returns distance between two coordinates
- * @param {Object[]} cord1 - 1st [lat, lon] pair
- * @param {Object[]} cord2 - 2nd [lat, lon] pair
+ * @param {Object[]} c1 - 1st [lat, lon] pair
+ * @param {Object[]} c2 - 2nd [lat, lon] pair
  */
-export default function(cord1, cord2) {
-  const angle = getCentralAngle(cord1, cord2)
+export default function(c1, c2) {
+  const angle = getCentralAngle(c1, c2)
   return 6371 * 2 * Math.atan2(Math.sqrt(angle), Math.sqrt(1 - angle))
 }
