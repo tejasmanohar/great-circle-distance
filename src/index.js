743522a24949e4c6634dd0684ae077ac3ee9ba4c
Tejas Manohar <me@tejas.io>
2015-10-24 06:28:18 -0500

    convert degrees -> radians on user data

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@
 import fs from 'fs'
 import filter from './input/filter'
 import getDistance from './math/distance'
+import { radians } from './math/angle'
 
 /**
  * Runner
@@ -14,8 +15,8 @@ import getDistance from './math/distance'
 (args => filter({
   stream: fs.createReadStream(args[0] || 'customers.txt'),
   fn: c => !!(getDistance(
-    [c.latitude, c.longitude],
-    args[1] || [53.3381985, -6.2592576]
+    [c.latitude, c.longitude].map(radians),
+    args[1] || [0.930927180905, -0.109244654]
   ) > (args[2] || 100)),
   cb: res => console.log(res)
 }))(process.argv.splice(2))
---
+++ src/math/angle.js
@@ -0,0 +1,8 @@
+
+/**
+ * Converts degrees to radians
+ * @param {number} degrees - measure in degrees
+ */
+export function radians(degrees) {
+  return degrees * (Math.PI / 180)
+}
