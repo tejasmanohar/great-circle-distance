56aba6b6dba752a0931e1b576beafbbebcfa2c2c
Tejas Manohar <me@tejas.io>
2015-10-24 06:48:52 -0500

    built for intercom- no need to parse that

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import getDistance from './math/distance'
   stream: createReadStream(args[0] || 'customers.txt'),
   fn: c => !!(getDistance(
     [c.latitude, c.longitude].map(radians),
-    args[1] || [0.930927180905, -0.109244654]
-  ) < (args[2] || 100)),
+    [0.930927180905, -0.109244654]
+  ) < (args[1] || 100)),
   cb: res => console.log(res)
 }))(process.argv.splice(2))
