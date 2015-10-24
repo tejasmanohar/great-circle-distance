82af57c8a743cc2b4471df93489bc89144647edf
Tejas Manohar <me@tejas.io>
2015-10-24 06:43:16 -0500

    fix order for operations w/ ( ) ranking

--- src/index.js
+++ src/index.js
@@ -17,6 +17,6 @@ import { radians } from './math/angle'
   fn: c => !!(getDistance(
     [c.latitude, c.longitude].map(radians),
     args[1] || [0.930927180905, -0.109244654]
-  ) > (args[2] || 100)),
+  ) < (args[2] || 100)),
   cb: res => console.log(res)
 }))(process.argv.splice(2))
--- src/math/distance.js
+++ src/math/distance.js
@@ -5,9 +5,9 @@
  * @param {Object[]} c2 - 2nd [lat, lon] pair
  */
 function getCentralAngle(c1, c2) {
-  return Math.sin(c2[0] - c1[0] / 2) ** 2 +
+  return Math.sin((c2[0] - c1[0]) / 2) ** 2 +
          Math.cos(c1[0]) * Math.cos(c2[0]) *
-         Math.sin(c2[1] - c1[1] / 2) ** 2
+         Math.sin((c2[1] - c1[1]) / 2) ** 2
 }
 
 /**
